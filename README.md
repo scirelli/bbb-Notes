929a12dfdacbc49e6eb0299e7035d9b67c672c37
scirelli <scirelli+git@gmail.com>
2022-07-15 09:57:11 -0400

    updating

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # bbb-Notes
 Beaglebones Black Notes
+
+## Wifi
+cat /etc/network/interface
+cat /etc/wpa_supplicant/wpa_supplicant.conf
+#### UI
+nmtui
+<!-- The following network interfaces were found in /etc/network/interfaces -->
+<!-- which means they are currently configured by ifupdown: -->
+<!-- - default -->
+<!-- - wlan0 -->
+<!-- If you want to manage those interfaces with NetworkManager instead -->
+<!-- remove their configuration from /etc/network/interfaces. -->
+
+## Reference
+* [Setup Wifi](https://askubuntu.com/a/833894/69928)
+  * [Wifi priority](https://raspberrypi.stackexchange.com/questions/58304/how-to-set-wifi-network-priority)
--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ useradd --create-home --shell /bin/bash --home "$userHomes/$userName" --password
 passwd --expire "$userName"
 echo alias=\'ls -lah\' > "$userHomes/$userName/.bash_aliases"
 usermod -a -G kmem,dialout,cdrom,floppy,audio,dip,video,plugdev,bluetooth,netdev,i2c,xenomai,tisdk,docker,iio,spi,remoteproc,eqep,pwm,gpio "$userName"
+ln -s "/media/sdcard/home/$userName" "/home/$userName"
 
 # == setup swap space
 swapon --show
@@ -60,12 +61,92 @@ eval "$(pyenv init --path)"
 eval "$(pyenv virtualenv-init -)"
 EOF
 
-sudo -H -u cck bash -c 'pyenv install 3.10.4'
-sudo -H -u cck bash -c 'pyenv global 3.10.4'
+runuser -l cck -c -- env PYTHON_CONFIGURE_OPTS="--enable-shared CC=clang" pyenv install 3.10.4
+runuser -l cck -c -- pyenv global 3.10.4
+#sudo -H -u cck bash -c 'pyenv install 3.10.4'
+#sudo -H -u cck bash -c 'pyenv global 3.10.4'
 
-# == WiFi setup
-apt install -y linux-headers-4.19.94-ti-r73 dkms bc
-git clone https://github.com/brektrou/rtl8821CU.git /tmp/rtl8821CU
-cd /tmp/rtl8821CU || exit
-./dkms-install.sh
-modprobe 8821cu
+## == WiFi setup
+# apt install -y linux-headers-4.19.94-ti-r73 dkms bc
+# git clone https://github.com/brektrou/rtl8821CU.git /tmp/rtl8821CU
+# cd /tmp/rtl8821CU || exit
+# ./dkms-install.sh
+# modprobe 8821cu
+
+# Install and update some packages
+apt install -y build-essential gdb strace network-manager vim git wpasupplicant sudo systemd screen gzip ca-certificates bluetooth apt xxd rsyslog openssl udev locales file git-man connman dbus distro-info-data clang libtool-bin libpython3-dev
+
+
+# File sharing if you're not comfortable working through ssh and vim
+apt install samba
+mkdir -p /home/cck/Projects
+mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
+cat <<EOF > /etc/samba/smb.conf
+#======================= Global Settings =======================
+[global]
+security = user
+allow insecure wide links = yes
+   workgroup = WORKGROUP
+   log file = /var/log/samba/log.%m
+   max log size = 1000
+   logging = file
+   panic action = /usr/share/samba/panic-action %d
+   server role = standalone server
+   obey pam restrictions = yes
+   unix password sync = yes
+   passwd program = /usr/bin/passwd %u
+   passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
+   pam password change = yes
+   map to guest = bad user
+   usershare allow guests = yes
+
+#======================= Share Definitions =======================
+[homes]
+   comment = Home Directories
+   browseable = no
+   read only = no
+   create mask = 0700
+   directory mask = 0700
+   valid users = %S
+
+[sambashare]
+    comment = Samba on Debian
+    path = $userHomes/$userName/sambashare
+    read only = no
+    browsable = yes
+    writeable = yes
+    follow symlinks = yes
+    wide links = yes
+    valid users = cck
+    create mask = 0644
+    directory mask = 0755
+EOF
+service smbd restart
+echo "$userName" SMB account
+smbpasswd -a $userName
+
+# The following network interfaces were found in /etc/network/interfaces
+# which means they are currently configured by ifupdown:
+# - default
+# - wlan0
+# If you want to manage those interfaces with NetworkManager instead
+# remove their configuration from /etc/network/interfaces.
+
+
+# https://developpaper.com/building-vim-from-source-code/
+git clone https://github.com/vim/vim.git /tmp/vim
+cd /tmp/vim || exit 1
+./configure \
+        --with-features=huge \
+        --enable-multibyte \
+        --enable-rubyinterp \
+        --enable-perlinterp \
+        --enable-luainterp \
+        --enable-pythoninterp \
+        --with-python-config-dir=/usr/lib/python2.7/config-arm-linux-gnueabihf/ \
+        --enable-python3interp \
+        --with-python3-config-dir=/media/sdcard/home/cck/.pyenv/versions/3.10.4/lib/python3.10/config-3.10-arm-linux-gnueabihf \
+        --enable-cscope \
+        --prefix=/usr
+EOF
+make && make install
